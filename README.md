081fe062011e93a8b6d74e7a4d6329440387c335
Pornpavee Seri-umnuoy <pornpavee.se@ku.th>
2019-05-30 00:01:35 +0700

    simple webpage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## Install
+## How to run
 
 1.) npm i
+2.) node script.js 
+3.) visit at `localhost:1234`
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+var path = require('path');
+var fs = require('fs');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -9,24 +11,40 @@ var connection = mysql.createConnection({
     database: 'perfectoDB'
 });
 
+app.use('/cssFiles', express.static(__dirname + '/assets'))
+
 connection.connect(function(error){
-    if(!!error){
+    if(error){
         console.log('Error');
     }else{
         console.log('Connected');
     }
 });
 
-app.get('/', function(req, resp){
+app.get('/', function(req, res){
     // mysql here
-    connection.query("SELECT * FROM Books", function(error, rows, fields){
-        if(error){
-            console.log('Error in query');
-        }else{
-            console.log('Successful query\n');
-            console.log(rows);
+    // connection.query("SELECT * FROM Books", function(error, rows, fields){
+    //     if(error){
+    //         console.log('Error in query');
+    //     }else{
+    //         console.log('Successful query\n');
+    //         console.log(rows);
+    //     }
+    // }),
+    res.sendFile('index.html', {root: path.join(__dirname, './files')});
+
+});
+
+app.get(/^(.+)$/, function(req, res){
+    try{
+        if(fs.statSync(path.join(__dirname, './files', req.params[0]+'.html')).isFile()){
+            res.sendFile(req.params[0]+'.html', {root: path.join(__dirname, './files')});
         }
-    })
+    }catch(err){
+        console.log(err)
+        res.sendFile('404.html', {root: path.join(__dirname, './files')});
+    }
+    
 });
 
 app.listen(1234);
