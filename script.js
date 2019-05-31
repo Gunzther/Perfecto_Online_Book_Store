895297a58494b2c0e330716d1f66f1db7aecd634
Pornpavee Seri-umnuoy <pornpavee.se@ku.th>
2019-05-31 13:35:17 +0700

    multiple page

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getConnection() {
   return mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Mokusa@12", //if need, put your password here
+    password: "", //if need, put your password here
     database: "perfectoDB"
   });
 }
